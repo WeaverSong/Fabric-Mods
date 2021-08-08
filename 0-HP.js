b7e9d239f1fcea43dace240ebf87eac019c0595f
WeaverSong <Legozipper@gmail.com>
2021-08-08 16:05:53 -0600

    Added comments

--- 0-HP.js
+++ 0-HP.js
@@ -1,10 +1,27 @@
-Fabric.Wrap(Character.prototype, "Character");
+//Example challenge mod. Caps the player's max HP at 1.
 
-Fabric.subscribe(this, 'post.Character.updateStats', (i) =>
-{
-    if (i.this.type.name === 'Player')
+
+//First wrap the required pieces. Note that Character is a es5 class, so we wrap Character.prototype, with the name "Character.prototype"
+//Also, note that it is important to give it the same name as it was originally, because Fabric will ignore requests to wrap things it has seen before, even if you want it wrapped under a new name.
+Fabric.Wrap(Character.prototype, "Character.prototype");
+
+//Define the mod as an object. This mod is very simple, and only has one function inside the object. We still want to wrap it in an object so it doesn't collide with other mods who want to use the same hooks
+const ZeroHP = {
+    init: function ()
     {
-        i.this.maxHp = 1;
-        i.this.currentHp = Math.min(1, i.this.currentHp);
+        //Subscribe to a Fabric event within the "Character.prototype" that we wrapped earlier.
+        Fabric.subscribe(this, 'post.Character.prototype.updateStats', (i) =>
+        {
+            //Notice how it is `i.this` instead of just `this`
+            //`this` is still the mod object, so you can still call other functions within your object. `i.this` is the context of the function the event came from.
+            if (i.this.type.name === 'Player')
+            {
+                i.this.maxHp = 1;
+                i.this.currentHp = Math.min(1, i.this.currentHp);
+            }
+        });
     }
-});
+};
+
+//Finally, call the init function for our mod to start it up. Comment out this line to disable the mod.
+ZeroHP.init();
--- Drow-Race.js
+++ Drow-Race.js
@@ -1,22 +1,41 @@
+//Example race mode. Adds a 'Drow' race.
+
+//First wrap the required pieces, with the name "gs"
+//Also, note that it is important to give it the same name as it was originally, because Fabric will ignore requests to wrap things it has seen before, even if you want it wrapped under a new name.
 Fabric.Wrap(gs, "gs");
 
-Fabric.subscribe(this, 'post.gs.createPlayerRaces', () =>
-{
-    gs.playerRaces.Drow = {
-        name: "Drow",
-        niceName: "Drow",
-        attributes: { strength: -2, dexterity: 0, intelligence: 0 },
-        effect: function (character) {
-            character.bonusProjectileRange += 1;
-        },
-        desc: function () {
-            var str = 'Drow\n';
-            str += 'Medium size\n';
-            str += '-2 strength\n';
-            str += '+1 range';
-            return str;
-        }
-    };
+//Define the mod as an object. This mod is very simple, and only has one function inside the object. We still want to wrap it in an object so it doesn't collide with other mods who want to use the same hooks
+const DrowRace = {
+    init: function ()
+    {
+        //Subscribe to an event withing the 'gs' object that we wrapped earlier.
+        //Note that this is the 'post' event, not the 'pre' event. This is due to how the function works, and unlike the old modloader Fabric can't avoid that.
+        Fabric.subscribe(this, 'post.gs.createPlayerRaces', () =>
+        {
+            //Other than using a different event, the only differences are that we need a `name` and `niceName` properties, and that we have to add it to the race list manually. 
+            gs.playerRaces.Drow = {
+                name: "Drow",
+                niceName: "Drow",
+                attributes: { strength: -2, dexterity: 0, intelligence: 0 },
+                effect: function (character)
+                {
+                    character.bonusProjectileRange += 1;
+                },
+                desc: function ()
+                {
+                    var str = 'Drow\n';
+                    str += 'Medium size\n';
+                    str += '-2 strength\n';
+                    str += '+1 range';
+                    return str;
+                }
+            };
+            //Note that we are adding it to the race list manually - because we are doing this we also get to choose where it goes in the list. I could have done something fancy to find the Elf race and put it right after it, even if it gets moved, but I didn't bother here, just hardcoding the index to put it at.
+            //You could also use `.push` to just add it to the end of the list.
+            gs.playerRaceList.splice(5, 0, gs.playerRaces.Drow);
+        });
+    }
+};
 
-    gs.playerRaceList.splice(5, 0, gs.playerRaces.Drow);
-});
+//Finally, call the init function for our mod to start it up. Comment out this line to disable the mod.
+DrowRace.init();
